97f80e5d78c64c0c2d931c516296de21eb00fae6
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2026-06-14 18:06:43 -0400

    Refactor API_BASE URL in Movies.js for consistency and enhance Navigation component with a subtitle

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -6,12 +6,16 @@ export default function Navigation() {
 		<nav className="flex items-center justify-between px-6 py-4 bg-[#E5D1A3] shadow-sm">
 			<div className="flex items-center gap-2">
 				<MdMovieEdit className="text-[#480902] text-3xl" />
+
 				<Link to="/">
 					<h1 className="text-2xl font-semibold text-[#480902] tracking-tight">
 						Watchlist
 					</h1>
 				</Link>
 			</div>
+			<h2 className="text-xl font-semibold text-[#480902] tracking-tight">
+				Your Movies, Your List.
+			</h2>
 		</nav>
 	);
 }
